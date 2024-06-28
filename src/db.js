2379ebcf33f08fe6a961e4e41362a0c5b32bac0b
Isaiah-Botwe <135121431+Isaiah-Botwe@users.noreply.github.com>
2024-06-28 23:33:14 +0000

    rewrote db connector

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 
-// I have made a change
+// I have made a change to this file 
 const prisma = new PrismaClient();
 
 module.exports = { prisma };
